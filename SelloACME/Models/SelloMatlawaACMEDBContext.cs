073db06076a7537e20db1e910be7b995755533b4
Sello Patrick Matlawa <Sello.Matlawa@eoh.com>
2019-11-21 14:29:45 +0200

    Add project files.

---
+++ SelloACME/Models/SelloMatlawaACMEDBContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace SelloACME.Models
+{
+    public partial class SelloMatlawaACMEDBContext : DbContext
+    {
+        public SelloMatlawaACMEDBContext()
+        {
+        }
+
+        public SelloMatlawaACMEDBContext(DbContextOptions<SelloMatlawaACMEDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employee { get; set; }
+        public virtual DbSet<Person> Person { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                optionsBuilder.UseSqlServer("Server=DESKTOP-2N1K87V;Database=SelloMatlawaACMEDB;Trusted_Connection=True;");
+//            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.EmployeeId).ValueGeneratedNever();
+
+                entity.Property(e => e.EmployedDate).HasColumnType("date");
+
+                entity.Property(e => e.EmployeeNum)
+                    .IsRequired()
+                    .HasMaxLength(16);
+
+                entity.Property(e => e.Terminated).HasColumnType("date");
+
+                entity.HasOne(d => d.Person)
+                    .WithMany(p => p.Employee)
+                    .HasForeignKey(d => d.PersonId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Employee_Person1");
+            });
+
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.Property(e => e.PersonId).ValueGeneratedNever();
+
+                entity.Property(e => e.BirthDate).HasColumnType("date");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(128);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(128);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ SelloACMEIntegrationTests/EmployeesIntegrationTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SelloACMEIntegrationTests
+{
+    class EmployeesIntegrationTests
+    {
+
+    }
+}
